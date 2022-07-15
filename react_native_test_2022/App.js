4247b6a028055d1bda2544dbfe2dfadd2c46b361
nuevo-rama <ramiromelero@gmail.com>
2022-07-15 10:14:42 -0300

    image source uri

--- react_native_test_2022/App.js
+++ react_native_test_2022/App.js
@@ -1,21 +1,36 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!</Text>
+      <Text style={styles.text}>Hello World!</Text>
       <StatusBar style="auto" />
+
+      <Image 
+        style={styles.image}
+        source={{uri: 'https://raw.githubusercontent.com/nuevo-rama/nuevo-rama.github.io/main/images/crb-3-nr.jpg'}}
+        
+      />
+
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    flex: 1, //center
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontSize: 30,
+  },
+  image: {
+    height: 200,
+    width: 200,
+  }
+  
 });
