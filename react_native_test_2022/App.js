033fc24de1402e0060d3c93e0316ef8e2f66a984
nuevo-rama <ramiromelero@gmail.com>
2022-07-15 13:36:01 -0300

    TouchableOpacity

--- react_native_test_2022/App.js
+++ react_native_test_2022/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
@@ -11,8 +11,15 @@ export default function App() {
       <Image 
         style={styles.image}
         source={{uri: 'https://raw.githubusercontent.com/nuevo-rama/nuevo-rama.github.io/main/images/crb-3-nr.jpg'}}
-        
       />
+      <TouchableOpacity 
+        style={styles.button}
+        onPress={() => Alert.alert('Simple Button pressed')}
+        >
+        
+        <Text>Press me</Text>
+        
+      </TouchableOpacity>
 
     </View>
   );
@@ -26,11 +33,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    margin: 10,
     fontSize: 30,
   },
   image: {
+    margin: 10,
     height: 200,
     width: 200,
-  }
+    borderRadius: 100,
+  },
+  button: {
+    margin: 10,
+    backgroundColor: 'grey',
+    width: 150,
+    borderRadius: 10,
+    padding: 5,
+    textAlign: 'center',
+  },
   
 });
